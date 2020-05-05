ab4fc1b65033620d5443caf436b3f3566e630d73
Alexander Thiem <thiemalexs@gmail.com>
2020-05-05 16:21:39 -0700

    improved fall detection algorithm

--- fall-detector/fall-detector.ino
+++ fall-detector/fall-detector.ino
@@ -4,16 +4,16 @@
 #include <Wire.h>
 
 // pins used for LCD
-#define LCD_RS 8
-#define LCD_EN 3
-#define LCD_D4 4
-#define LCD_D5 5
-#define LCD_D6 6
-#define LCD_D7 7
+#define LCD_RS 3
+#define LCD_EN 4
+#define LCD_D4 5
+#define LCD_D5 6
+#define LCD_D6 7
+#define LCD_D7 8
 
-#define FALL_TIME 250              // time to detect a fall after a high acceleration in milliseconds
-#define FALL_HIGH_THRESHOLD 30000  // high acceleration threshold to check for a fall
-#define FALL_LOW_THRESHOLD 1000    // low acceleration threshold to detect a fall
+#define FALL_TIME 25               // time to detect a fall after a high acceleration in milliseconds
+#define FALL_HIGH_THRESHOLD 25000  // high acceleration threshold to check for a fall
+#define FALL_LOW_THRESHOLD 3000    // low acceleration threshold to detect a fall
 
 // offsets found by holding the accelerometer still
 #define ACCEL_X_OFFSET -16000
@@ -26,13 +26,12 @@ MPU6050 accelgyro;
 
 // acceleration values from accelerometer
 int16_t ax, ay, az;
-
+bool negX = false;
 // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
 // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
 // not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
+// #define OUTPUT_READABLE_ACCELGYRO
 
-unsigned long counter = 0;
 unsigned long fallTimer = 0;
 
 void setup() {
@@ -57,41 +56,41 @@ void setup() {
 void loop() {
     // read raw accel/gyro measurements from device
     //accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
- 
+    negX = false;
     accelgyro.getAcceleration(&ax, &ay, &az);
     ax = ax + ACCEL_X_OFFSET;
     ay = ay + ACCEL_Y_OFFSET;
     az = az + ACCEL_Z_OFFSET;
-    long accel = sqrt((long)ax*ax + (long)ay*ay + (long)az*az);
+    unsigned long accel = sqrt((long)ax*ax + (long)ay*ay + (long)az*az);
 
-    if (millis() - counter > 100) {
-      counter = millis();
-      // debugging prints
-      #ifdef OUTPUT_READABLE_ACCELGYRO
-          Serial.print("a/g:\t");
-          Serial.print(ax); Serial.print("\t");
-          Serial.print(ay); Serial.print("\t");
-          Serial.print(az); Serial.print("\t");
-          Serial.println(accel);
-      #endif
-      if (accel > FALL_HIGH_THRESHOLD) {
-        fallTimer = millis();
-        // check for low acceleration after breaking high threshold for FALL_TIME
-        while (millis() - fallTimer < FALL_TIME) {
-          accelgyro.getAcceleration(&ax, &ay, &az);
-          accel = sqrt((long)ax*ax + (long)ay*ay + (long)az*az);
-          // if low acceleration is passed, a fall is detected
-          if (accel < FALL_LOW_THRESHOLD) {
-            Serial.println("Fall detected");
-            lcd.print("Fall detected");
-            delay(2000);
-            lcd.clear();
-            break;
-          }
-          fallTimer = millis();
+    // debugging prints
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.println(accel);
+    #endif
+    if (accel > FALL_HIGH_THRESHOLD) {
+      fallTimer = millis();
+      // check for low acceleration after breaking high threshold for FALL_TIME
+      while (millis() - FALL_TIME < fallTimer) {
+        accelgyro.getAcceleration(&ax, &ay, &az);
+        accel = sqrt((long)ax*ax + (long)ay*ay + (long)az*az);
+        if(!negX && ax < 0) {
+          negX = true;
+        }
+        // if low acceleration is passed, a fall is detected
+        if ((accel != 0) && (accel < FALL_LOW_THRESHOLD) && negX) {
+          Serial.println("Fall detected");
+          lcd.print("Fall detected");
+          delay(7000);
+          lcd.clear();
+          break;
         }
       }
     }
+    
 
     
 }
